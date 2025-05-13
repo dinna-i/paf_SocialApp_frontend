535e1369d0165bc7f407e093dd4aff4f6c12d70b
aloshash <aloka.shash@gmail.com>
2025-05-14 01:17:55 +0530

    lib folder updated

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/*
+// New utility functions added from the provided code
+
+// Format percentage to rounded value with % sign
+export const formatPercentage = (percentage: number): string => {
+  return `${Math.round(percentage)}%`;
+};
+
+// Alternative class name utility that joins strings
+export const classNames = (...classes: string[]): string => {
+  return classes.filter(Boolean).join(' ');
+};
+
+// Debounce function for performance optimization
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): ((...args: Parameters<T>) => void) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  
+  return (...args: Parameters<T>) => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}; */
